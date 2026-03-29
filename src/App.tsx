5e0eeb8e2de8746de8d303a1a17df3cb336fe956
litelp <lponomareva97@yandex.ru>
2026-03-29 16:27:18 +0400

    refactor: add react-router and create a basic app structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
-import './App.css';
+import styles from './App.module.scss';
+import { Footer } from '@components/Footer/Footer';
+import { Header } from '@/components/Header/Header';
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <div className={styles.app}>
+        <Header></Header>
+      </div>
+      <Footer></Footer>
+    </>
+  );
 }
 
 export default App;
