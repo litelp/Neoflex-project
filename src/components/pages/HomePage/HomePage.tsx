35c7470fd8616239b8d2d57e196ade8874e79f2b
litelp <lponomareva97@yandex.ru>
2026-03-30 16:22:52 +0400

    feat: add features section on home page

--- src/components/pages/HomePage/HomePage.tsx
+++ src/components/pages/HomePage/HomePage.tsx
@@ -4,6 +4,7 @@ import creditCard_1 from '@assets/images/cardImage1.png';
 import creditCard_2 from '@assets/images/cardImage2.png';
 import creditCard_3 from '@assets/images/cardImage3.png';
 import creditCard_4 from '@assets/images/cardImage4.png';
+import featuresImg from '@assets/images/Illustration.svg';
 
 export function HomePage() {
   return (
@@ -27,6 +28,34 @@ export function HomePage() {
             <img src={creditCard_4} alt="Credit card design option 4" />
           </li>
         </ul>
+      </section>
+      <section className={styles.features}>
+        <img
+          className={styles['features__icon']}
+          src={featuresImg}
+          alt="Features"
+        />
+        <div className={styles['features__text']}>
+          <h2 className={styles['features__title']}>
+            We Provide Many Features You Can Use
+          </h2>
+          <p className={styles['features__paragraph']}>
+            You can explore the features that we provide with fun and have their
+            own functions each feature
+          </p>
+          <ul className={styles['features__list']}>
+            <li className={styles['features__item']}>
+              Powerfull online protection
+            </li>
+            <li className={styles['features__item']}>
+              Cashback without borders
+            </li>
+            <li className={styles['features__item']}>Personal design</li>
+            <li className={styles['features__item']}>
+              Work anywhere in the world
+            </li>
+          </ul>
+        </div>
       </section>
     </main>
   );
