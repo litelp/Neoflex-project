a7198f8907151910ab7e79a3081520de7ccc40a7
litelp <lponomareva97@yandex.ru>
2026-04-27 18:19:08 +0400

    feat: add tooltips on tooltip section and add react-hook-form package

--- src/components/pages/LoanPage/sections/TooltipSection/TooltipSection.tsx
+++ src/components/pages/LoanPage/sections/TooltipSection/TooltipSection.tsx
@@ -1,6 +1,7 @@
 import { Button } from '@/components/Button/Button';
 import styles from './TooltipSection.module.scss';
 import creditCard from '@assets/images/loan_card.png';
+import { Tooltip } from './Tooltip/Tooltip';
 
 export function TooltipSection() {
   return (
@@ -13,18 +14,24 @@ export function TooltipSection() {
       </p>
       <ul className={styles['tooltip__benefits-list']}>
         <li className={styles['tooltip__benefit-item']}>
-          <p className={styles['tooltip__benefit-title']}>Up to 160 days</p>
-          <p className={styles['tooltip__benefit-text']}>No percent</p>
+          <Tooltip text="When repaying the full debt up to 160 days.">
+            <p className={styles['tooltip__benefit-title']}>Up to 160 days</p>
+            <p className={styles['tooltip__benefit-text']}>No percent</p>
+          </Tooltip>
         </li>
         <li className={styles['tooltip__benefit-item']}>
-          <p className={styles['tooltip__benefit-title']}>Up to 600 000 ₽</p>
-          <p className={styles['tooltip__benefit-text']}>Credit limit</p>
+          <Tooltip text="Over the limit willaccrue percent">
+            <p className={styles['tooltip__benefit-title']}>Up to 600 000 ₽</p>
+            <p className={styles['tooltip__benefit-text']}>Credit limit</p>
+          </Tooltip>
         </li>
         <li className={styles['tooltip__benefit-item']}>
-          <p className={styles['tooltip__benefit-title']}>0 ₽</p>
-          <p className={styles['tooltip__benefit-text']}>
-            Card service is free
-          </p>
+          <Tooltip text="Promotion valid until December 31, 2022.">
+            <p className={styles['tooltip__benefit-title']}>0 ₽</p>
+            <p className={styles['tooltip__benefit-text']}>
+              Card service is free
+            </p>
+          </Tooltip>
         </li>
       </ul>
       <Button className={styles['tooltip__button']} text="Apply for card" />
